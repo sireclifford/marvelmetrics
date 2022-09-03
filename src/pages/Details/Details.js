47b90776777fb864f18d9b24b56899fc704f1887
sireclifford <dev.sire.clifford@gmail.com>
2022-09-03 19:34:46 +0000

    - Add test for reducers - fix detail component not rendering new movie id - modified movieslice file

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,22 +1,55 @@
 /* eslint-disable camelcase */
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import Movie from '../../components/movie/Movie';
+import { fetchMovieDetails } from '../../redux/MovieSlice';
 import Nav from '../../components/nav/Nav';
+import './Details.css';
 
 const Details = () => {
-  const status = useSelector((state) => state.movies.status);
-  const movies = useSelector((state) => state.movies.movies);
+  const dispatch = useDispatch();
+  const { detailStatus } = useSelector((state) => state.movies);
+  const movie = useSelector((state) => state.movies.selectedMovie);
   const { movie_id } = useParams();
-  const selectedMovie = movies.movies.filter((movie) => movie.genres.includes(movie_id));
+
+  useEffect(() => {
+    if (detailStatus === 'idle') {
+      dispatch(fetchMovieDetails(movie_id));
+    }
+  }, [movie_id, dispatch, detailStatus]);
 
   let content;
-  if (status === 'loading') {
+  if (detailStatus === 'loading') {
     content = <div>Loading...</div>;
-  } else if (status === 'succeeded') {
-    content = (
-      selectedMovie.map((movie) => <Movie key={movie.id} movie={movie} />));
+  } else if (detailStatus === 'succeeded') {
+    if (movie != null) {
+      content = (
+        <div className="movie-item">
+          <img
+            className="movie-image"
+            src={movie.movie.medium_cover_image}
+            alt={movie.movie.title}
+          />
+          <h2 className="movie-title">{movie.movie.title}</h2>
+          <div className="movie-heading">
+            <p className="genre">
+              Genre:
+              {movie.movie.genres[0]}
+            </p>
+
+            <div className="ratings">
+              <p className="seeds">Rating</p>
+              <p className="seeds-value">{movie.movie.rating}</p>
+            </div>
+          </div>
+          <div className="movie-details">
+            <p className="descrition">{movie.movie.description_full}</p>
+          </div>
+        </div>
+      );
+    }
+  } else if (detailStatus === 'failed') {
+    content = <div>Failed to load movie details</div>;
   }
   return (
     <>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Stats from '../../components/stats/Stats';
 import Genre from '../../components/genre/Genre';
 import Nav from '../../components/nav/Nav';
+import { SET_DETAIL_STATE } from '../../redux/MovieSlice';
 
-const Home = () => (
-  <>
-    <Nav />
-    <Stats />
-    <Genre />
-  </>
-);
+const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(SET_DETAIL_STATE('idle'));
+  }, [dispatch]);
+
+  return (
+    <>
+      <Nav />
+      <Stats />
+      <Genre />
+    </>
+  );
+};
 
 export default Home;
